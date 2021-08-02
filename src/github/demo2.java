16e48ee06888641308d2947a4c7857d2eed2b291
Ram <Ram@DESKTOP-OO8CKP7>
2021-08-02 13:04:38 +0530

    [mandakini]:github repository

---
+++ src/github/demo2.java
@@ -0,0 +1,21 @@
+package github;
+
+public class demo2 {
+public static void main(String[] args) {
+
+	
+	
+	System.out.println("hlo gm");
+	System.out.println("hlo gm");
+	System.out.println("hlo gm");
+	System.out.println("hlo gm");
+	System.out.println("hlo gm");
+	System.out.println("hlo gm");
+
+
+	
+}
+
+
+
+}
